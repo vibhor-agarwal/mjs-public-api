5f104a29383ef9d78769795248d1d07754b79aff
Vibhor Agarwal <Vibhor.Agarwal@ihsmarkit.com>
2018-11-28 13:42:37 +0530

    added all possible startup options

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 ## Use your own data
 
 Fork it and change `db.json` values or create a repo with a `db.json` file.
+
+## Commands
+
+node index.js
+node server.js
+npm run server
+npm start
---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+
+'use strict';
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const fs = require('fs');
+
+let jsonServerConfigJson = fs.readFileSync('json-server.json');
+let jsonServerConfig = JSON.parse(jsonServerConfigJson);
+let jsonServerPort = jsonServerConfig.port;
+console.log('Using port: ' + jsonServerPort);
+
+let port = Number.parseInt(jsonServerPort);
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+    console.log('JSON Server is running')
+});
